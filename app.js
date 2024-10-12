df20b3f2113d9911533ba3e5808b7a00dbab8e18
Uygar <komifb90@hotmail.com>
2024-10-12 20:49:34 +0300

    user test

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Runnings!");
+});
+
+app.get("/user", (req, res) => {
+  res.send("User Runnings!");
 });
 
 app.listen(PORT, () => {
