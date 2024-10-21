a18257febd72220c2213a9c30e045974918fd318
Uygar Eren <komifb@hotmail.com>
2024-10-21 14:52:14 +0300

    github action testing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 
 app.get("/", (req, res) => {
-  res.send("Running 2!");
+  res.send("Running 33!");
 });
 
 const PORT = 3000;
